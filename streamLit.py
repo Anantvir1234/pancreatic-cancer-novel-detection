fa8583a23bf9183e57dd6ad03efa2ede8cb1e72e
Anantvir1234 <141280073+Anantvir1234@users.noreply.github.com>
2024-02-20 21:42:39 -0800

    Update streamLit.py

--- streamLit.py
+++ streamLit.py
@@ -72,7 +72,8 @@ else:
     
     input_df = user_input_features()
     if input_df is not None:
-        if st.button("Process values", disabled=(input_df is None)):
+        process_button_disabled = "error" in st.session_state or input_df is None
+        if st.button("Process values", disabled=process_button_disabled):
             predictions = predict(input_df)
             st.subheader("Final Results:")
             cancer_detected = bool(predictions[0])
