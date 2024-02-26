e142386568f67528174344124451225474129bc1
Anantvir1234 <141280073+Anantvir1234@users.noreply.github.com>
2024-02-26 00:04:58 -0800

    Update streamLit.py

--- streamLit.py
+++ streamLit.py
@@ -9,6 +9,9 @@ import os
 venv_dir = "/home/adminuser/venv"  # Replace with the correct path to your virtual environment
 
 # Check if xgboost is installed
+import xgboost as xgb
+print(xgb.__version__)
+
 try:
     import xgboost
 except ImportError:
