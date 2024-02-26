cba7105cea850e06084ed990a4eebe5a8f351a2a
Anantvir1234 <141280073+Anantvir1234@users.noreply.github.com>
2024-02-25 23:11:05 -0800

    Update streamLit.py

--- streamLit.py
+++ streamLit.py
@@ -2,26 +2,15 @@ import streamlit as st
 import pandas as pd
 import pickle
 
+
 def predict(data, model_path="model_xgb.sav"):
     try:
         with open(model_path, 'rb') as model_file:
             clf = pickle.load(model_file)
-            predictions = clf.predict(data)
+        predictions = clf.predict(data)
         return predictions
     except Exception as e:
         return f"Error: {e}"
-
-def predict_pancreatic_cancer(data, model_path="model_xgb.sav"):
-    # Load the pre-trained model using pickle
-    with open(model_path, 'rb') as model_file:
-        clf = pickle.load(model_file)
-    features = ["REG1A", "creatinine", "TFF1", "LYVE1", "plasma_CA19_9", "REG1B", "age"]
-    input_data = data[features]
-
-    # Make probability predictions
-    prediction_probs = clf.predict_proba(input_data)
-
-    return prediction_probs
 
 # Title and description
 title = "Pancreatic Cancer Detection"
@@ -31,7 +20,6 @@ st.markdown("Detect pancreatic cancer through an uploaded CSV file.")
 
 # Upload CSV file
 uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-
 if uploaded_file is not None:
     # Load CSV data into a DataFrame
     df = pd.read_csv(uploaded_file)
@@ -42,7 +30,6 @@ if uploaded_file is not None:
 
     # Check for specific column names relevant to pancreatic cancer detection
     required_columns = ["REG1A", "creatinine", "TFF1", "LYVE1", "plasma_CA19_9", "REG1B", "age"]
-
     if all(col in df.columns for col in required_columns):
         st.subheader("Pancreatic Cancer Detection Results:")
 
@@ -52,10 +39,5 @@ if uploaded_file is not None:
             predictions = predict(df[required_columns])
             st.subheader("Final Results:")
             st.write("Pancreatic Cancer Detected" if any(predictions) else "Not Detected")
-
-            # In the processing section:
-            prediction_probs = predict_pancreatic_cancer(df)
-            st.subheader("Model Prediction Probabilities:")
-            st.write(prediction_probs[:, 1])  # Assuming 1 is the cancer class, adjust if needed
     else:
         st.warning("The uploaded CSV file does not have the expected column names for pancreatic cancer detection. Please check the file structure")
