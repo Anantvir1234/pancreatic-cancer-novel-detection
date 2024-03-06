d7d6e5f0fe50a25a6fd3ffac43469d5ffe809d60
Anantvir1234 <141280073+Anantvir1234@users.noreply.github.com>
2024-03-05 19:45:44 -0800

    Update streamLit.py

--- streamLit.py
+++ streamLit.py
@@ -2,130 +2,145 @@ import pandas as pd
 import joblib
 import sys
 import subprocess
+import streamlit as st
+from cryptography.fernet import Fernet
 
-# Install lightgbm if not already installed
+# Install required packages if not already installed
 try:
     import lightgbm
 except ImportError:
     subprocess.run([sys.executable, "-m", "pip", "install", "lightgbm"])
 
+try:
+    import cryptography
+except ImportError:
+    subprocess.run([sys.executable, "-m", "pip", "install", "cryptography"])
+
 # Forcefully upgrade pip
 subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
 
-# Now import Streamlit after pip has been upgraded
-import streamlit as st
-
 # Set page configuration
 title = "Pancreatic Cancer Detection"
-st.set_page_config(page_title=title)
+st.set_page_config(page_title=title, page_icon=":hospital:")
 
 # Define clf at the beginning of the script
 clf = None
 
+# Define the cryptographic key for encrypting patient data
+key = b'your_secret_key'  # Replace with a secure key
+
+cipher_suite = Fernet(key)
+
 def load_model(model_path="best_model_lgbm.sav"):
-    global clf  # Declare clf as a global variable
+    global clf
     try:
         clf = joblib.load(model_path)
         return clf
     except Exception as e:
         return f"Error loading model: {e}"
 
-# Rest of your code...
+def encrypt_data(data):
+    return cipher_suite.encrypt(data.encode())
 
-
-
-
-# Define clf at the beginning of the script
-clf = None
-
-def load_model(model_path="best_model_lgbm.sav"):
-    global clf  # Declare clf as a global variable
-    try:
-        clf = joblib.load(model_path)
-        return clf
-    except Exception as e:
-        return f"Error loading model: {e}"
+def decrypt_data(data):
+    return cipher_suite.decrypt(data).decode()
 
 def predict(data):
-    global clf  # Declare clf as a global variable
+    global clf
     try:
         predictions = clf.predict(data)
         return predictions
     except Exception as e:
         return f"Error making predictions: {e}"
 
-# Title and description
-st.header(title)
-st.markdown("Detect pancreatic cancer through an uploaded CSV file or input raw data.")
+# Separate login and create account page for doctors
+def login_or_create_account():
+    st.title("Doctor Login / Create Account")
+    st.markdown("Please enter your credentials to log in or create an account.")
 
-# Choose between uploading a CSV file or inputting raw data
-option = st.radio("Select an option:", ["Upload a CSV file", "Input Raw Data"])
+    # Implement doctor login and account creation logic here
+    # ...
 
-if option == "Upload a CSV file":
-    # Upload CSV file
-    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-    if uploaded_file is not None:
-        # Load CSV data into a DataFrame
-        df = pd.read_csv(uploaded_file)
-        st.subheader("Preview of the uploaded data:")
-        st.write(df.head().values.tolist())
+# Check if the doctor is logged in
+doctor_logged_in = st.checkbox("Doctor logged in")
 
-        # Check for specific column names relevant to pancreatic cancer detection
+if doctor_logged_in:
+    # Title and description
+    st.title(title)
+    st.markdown("Detect pancreatic cancer through an uploaded CSV file or input raw data.")
+
+    # Choose between uploading a CSV file or inputting raw data
+    option = st.radio("Select an option:", ["Upload a CSV file", "Input Raw Data"])
+
+    if option == "Upload a CSV file":
+        # Upload CSV file
+        uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+        if uploaded_file is not None:
+            # Load CSV data into a DataFrame
+            df = pd.read_csv(uploaded_file)
+            st.subheader("Preview of the uploaded data:")
+            st.write(df.head().values.tolist())
+
+            # Check for specific column names relevant to pancreatic cancer detection
+            required_columns = ["REG1A", "creatinine", "TFF1", "LYVE1", "plasma_CA19_9", "REG1B", "age", "sex"]
+            if all(col in df.columns for col in required_columns):
+                st.subheader("Pancreatic Cancer Detection Results:")
+
+                # Load model if not loaded
+                if clf is None:
+                    clf = load_model()
+
+                # Button for processing the uploaded file
+                if st.button("Process Uploaded File", key="process_uploaded_file"):
+                    # Get probabilities of positive class using the pre-trained model
+                    predictions_proba = predict(df[required_columns])
+                    threshold = 0.3  # You can adjust this threshold based on your model and requirements
+
+                    # Convert numpy array to Pandas Series
+                    predictions_proba_series = pd.Series(predictions_proba)
+
+                    # Convert the elements to float and fill NaN values with 0
+                    predictions_proba_numeric = pd.to_numeric(predictions_proba_series, errors='coerce').fillna(0)
+
+                    # Display model information
+                    st.subheader("Loaded Model Information:")
+                    st.write(clf)  # Display model information
+
+                    # Convert probabilities to binary predictions using the threshold
+                    predictions = (predictions_proba_numeric.astype(float) > threshold).astype(int)
+
+                    st.subheader("Final Results:")
+                    st.write("Pancreatic Cancer Detected" if any(predictions) else "Not Detected")
+
+            else:
+                st.warning("The uploaded CSV file does not have the expected column names for pancreatic cancer detection. Please check the file structure")
+
+    else:
+        # Input raw data
+        st.subheader("Please Input Features Value")
+
+        # Input numerical values for each column and biomarker
+        features_input = {}
         required_columns = ["REG1A", "creatinine", "TFF1", "LYVE1", "plasma_CA19_9", "REG1B", "age", "sex"]
-        if all(col in df.columns for col in required_columns):
-            st.subheader("Pancreatic Cancer Detection Results:")
 
-            # Load model if not loaded
-            if clf is None:
-                clf = load_model()
+        for column in required_columns:
+            if column == "age":
+                features_input[column] = st.number_input(f'{column} (greater than or equal to 1): ', min_value=1)
+            elif column == "sex":
+                features_input[column] = st.number_input(f'{column} (0 for Male, 1 for Female): ', min_value=0, max_value=1, format="%d")
+            else:
+                features_input[column] = st.number_input(f'{column}: ', min_value=0)
 
-            # Button for processing the uploaded file
-            if st.button("Process Uploaded File", key="process_uploaded_file"):
-                # Get probabilities of positive class using the pre-trained model
-                predictions_proba = predict(df[required_columns])
-                threshold = 0.3  # You can adjust this threshold based on your model and requirements
+        # Button for processing the inputted raw data
+        if st.button("Process Raw Data", key="process_raw_data"):
+            # Create a DataFrame with the input data
+            input_df = pd.DataFrame(features_input, index=[0])
 
-                # Convert numpy array to Pandas Series
-                predictions_proba_series = pd.Series(predictions_proba)
+            # Get predictions using the pre-trained model
+            predictions = predict(input_df[required_columns])
+            st.subheader("Final Results:")
+            st.write("Pancreatic Cancer Detected" if any(predictions) else "Not Detected")
 
-                # Convert the elements to float and fill NaN values with 0
-                predictions_proba_numeric = pd.to_numeric(predictions_proba_series, errors='coerce').fillna(0)
-
-                # Display model information
-                st.subheader("Loaded Model Information:")
-                st.write(clf)  # Display model information
-
-                # Convert probabilities to binary predictions using the threshold
-                predictions = (predictions_proba_numeric.astype(float) > threshold).astype(int)
-
-                st.subheader("Final Results:")
-                st.write("Pancreatic Cancer Detected" if any(predictions) else "Not Detected")
-
-        else:
-            st.warning("The uploaded CSV file does not have the expected column names for pancreatic cancer detection. Please check the file structure")
-
+# If the doctor is not logged in, show the login or create account page
 else:
-    # Input raw data
-    st.subheader("Please Input Features Value")
-
-    # Input numerical values for each column and biomarker
-    features_input = {}
-    required_columns = ["REG1A", "creatinine", "TFF1", "LYVE1", "plasma_CA19_9", "REG1B", "age", "sex"]
-
-    for column in required_columns:
-        if column == "age":
-            features_input[column] = st.number_input(f'{column} (greater than or equal to 1): ', min_value=1)
-        elif column == "sex":
-            features_input[column] = st.number_input(f'{column} (0 for Male, 1 for Female): ', min_value=0, max_value=1, format="%d")
-        else:
-            features_input[column] = st.number_input(f'{column}: ', min_value=0)
-
-    # Button for processing the inputted raw data
-    if st.button("Process Raw Data", key="process_raw_data"):
-        # Create a DataFrame with the input data
-        input_df = pd.DataFrame(features_input, index=[0])
-
-        # Get predictions using the pre-trained model
-        predictions = predict(input_df[required_columns])
-        st.subheader("Final Results:")
-        st.write("Pancreatic Cancer Detected" if any(predictions) else "Not Detected")
+    login_or_create_account()
